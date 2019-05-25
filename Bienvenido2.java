b99e46d942be6bb4c02254779e1a52a2e81a2171
Salva Ruiz <salvadorruizlozano@gmail.com>
2019-05-25 18:03:57 +0200

    programas

---
+++ Bienvenido2.java
@@ -0,0 +1,16 @@
+// Fig. 2.3: Bienvenido2.java
+// Imprimir una línea de texto con varias instrucciones.
+
+public class Bienvenido2
+{
+// el método main empieza la ejecución de la aplicación en Java
+public static void main( String args[] )
+{
+System.out.print( "\t\tBienvenido a " );
+System.out.println( "la programacion en Java!" );
+System.out.println( "Bienvenido\na\nla programacion\nenJava!" );
+System.out.printf( "%s\n%s\n","Bienvenido a", "la programacion en Java!" );
+System.out.printf( "%s\n%s\n",
+"Bienvenido a", "la programacion en Java!" );
+} // fin del método main
+} // fin de la clase Bienvenido2
